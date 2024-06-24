3e6568a63f215aaef37272c58e5e850c9cac4a1b
MCDonaldTrunks <luis_p456@outlook.com>
2024-06-24 00:44:47 -0700

    upload to git hub commit

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,8 +1,29 @@
-import {create} from 'zustand'
-import {mountStoreDevtool} from 'simple-zustand-devtools'
+import { create } from 'zustand'
+import { mountStoreDevtool } from 'simple-zustand-devtools'
 
 
 const useAuthStore = create((set, get) => ({
     allUserData: null,
+    loading: false,
+    user: () => ({
+        user_id: get().allUserData?.user_id || null,
+        username: get().allUserData?.username || null,
 
-}))
+    }),
+    setUser: (user) => set({
+        allUserData: user,
+
+    }),
+
+    setLoading: (loading) => set({ loading }),
+
+    isLoggedIn: () => get().allUserData !== null,
+
+}));
+
+if (import.meta.env.Dev) {
+    mountStoreDevtool("Store", useAuthStore);
+}
+
+export { useAuthStore }
+
